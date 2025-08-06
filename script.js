f9906051d9b9469c86cb29dd036da87dccadbc5f
BodaGalaJose <bodagalajose@gmail.com>
2025-08-07 00:26:04 +0200

    V10

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function setLanguage(lang) {
+  // Cambia el contenido traducido
   document.querySelectorAll('[data-' + lang + ']').forEach(function (el) {
     el.textContent = el.getAttribute('data-' + lang);
+  });
+
+  // Cambia el formulario visible
+  document.querySelectorAll('.form-container > div').forEach(function (form) {
+    form.style.display = form.getAttribute('data-lang') === lang ? 'block' : 'none';
   });
 }
